d079549d03d72d2bf996ba959697bbe06230d103
cauassouzza <caua.brasileiro@gmail.com>
2026-07-08 19:35:45 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Inclusão dos arquivos de cabeçalho do projeto
 #include "../include/menu.h"
 #include "../include/interface.h"
 #include "../include/torres.h"
 #include "../include/movimentos.h"
 #include "../include/automatico.h"
 
+// Função principal do programa
 int main()
 {
+    // Vetor que armazenará as três torres do jogo
     Torre torres[MAX_TORRES];
 
+    // Estrutura para armazenar o histórico dos movimentos
     HistoricoMovimentos historico;
 
+    // Variáveis de controle
     int opcao;
     int numDiscos;
     int totalMovimentos = 0;
 
+    // Exibe a tela inicial
     telaInicial();
 
+    // Mantém o menu funcionando até o usuário escolher sair
     do
     {
+        // Mostra o menu e recebe a opção escolhida
         opcao = menuPrincipal();
 
         switch(opcao)
         {
+            // Inicia o modo manual
             case 1:
             {
                 printf("Quantidade de discos (1 a 7): ");
                 scanf("%d", &numDiscos);
 
+                // Verifica se a quantidade de discos é válida
                 if(numDiscos < 1 || numDiscos > MAX_DISCOS)
                 {
                     printf("Quantidade invalida!\n");
@@ -37,23 +47,29 @@ int main()
                     break;
                 }
 
+                // Inicializa as torres com os discos
                 inicializarTorre(torres, numDiscos);
 
+                // Zera o histórico de movimentos
                 inicializarHistorico(&historico);
 
+                // Mostra as torres na tela
                 exibir_torre(torres, numDiscos);
 
+                // Parte do jogo manual ainda será integrada
                 printf("\nModo manual ainda sera integrado.\n");
 
                 system("pause");
                 break;
             }
 
+            // Inicia o modo automático
             case 2:
             {
                 printf("Quantidade de discos (1 a 7): ");
                 scanf("%d", &numDiscos);
 
+                // Valida a quantidade de discos
                 if(numDiscos < 1 || numDiscos > MAX_DISCOS)
                 {
                     printf("Quantidade invalida!\n");
@@ -61,8 +77,10 @@ int main()
                     break;
                 }
 
+                // Inicializa as torres
                 inicializarTorre(torres, numDiscos);
 
+                // Resolve automaticamente utilizando recursão
                 resolverAutomatico(
                     torres,
                     numDiscos,
@@ -72,14 +90,17 @@ int main()
                     &totalMovimentos
                 );
 
+                // Exibe as torres após a resolução
                 exibir_torre(torres, numDiscos);
 
-                printf("\nMovimentos: %d\n", totalMovimentos);
+                // Mostra a quantidade total de movimentos
+                printf("\nMovimentos realizados: %d\n", totalMovimentos);
 
                 system("pause");
                 break;
             }
 
+            // Mostra o highscore (quando implementado)
             case 3:
             {
                 printf("\nHighscore ainda sera integrado.\n");
@@ -88,12 +109,14 @@ int main()
                 break;
             }
 
+            // Encerra o programa
             case 4:
             {
                 telaFinal();
                 break;
             }
 
+            // Caso o usuário digite uma opção inválida
             default:
             {
                 printf("Opcao invalida!\n");
@@ -101,7 +124,7 @@ int main()
             }
         }
 
-    } while(opcao != 4);
+    } while(opcao != 4); // Repete o menu enquanto a opção não for sair
 
     return 0;
 }
